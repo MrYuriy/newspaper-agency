56f316974922dacd4b2ed5aa09fd4b7719c5069b
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-04-19 17:57:02 +0300

    Add debug_toolbar

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include("newspaper.urls", namespace="newspaper")),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
