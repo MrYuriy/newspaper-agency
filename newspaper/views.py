7c2e014db0015066f79475906de97a8507a4300d
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-04-19 14:59:58 +0300

    Add index and base  pages and view  configurate urls.py

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Topic, Newspaper, Redactor
 
-# Create your views here.
+
+def index(requests):
+    num_topics = Topic.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_redactor = Redactor.objects.count()
+
+    num_visits = requests.session.get("num_visits", 0)
+    requests.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_topics": num_topics,
+        "num_newspapers": num_newspapers,
+        "num_redactor": num_redactor,
+        "num_visits": num_visits
+    }
+    return render(requests, "newspaper/index.html", context=context)
